ce297e6eaa0058a8280c27dc67729b4e708f1541
mahfuz-antor <antorcnj2014@gmail.com>
2024-12-29 22:57:47 +0600

    nested conditional pacticing

--- secondDay.js
+++ secondDay.js
@@ -33,3 +33,38 @@ if(time < 10) {
     thirdGreetings = '3rd...Good Evening!';
     console.log(thirdGreetings);
 }
+
+// 4th part nested conditional... 
+
+const gender = 'female';
+const age = 22;
+const student = true;
+const futureTarget = true;
+const entrepeneur = true;
+
+if(gender == 'male') {
+    console.log('Welcome in the male zone');
+    if(age > 20) {
+        console.log('Your age is enough  to jump the next plane!');
+        if(student == true) {
+            console.log('You are a student. It"s right time to start');
+            if(entrepeneur == true) {
+                console.log('You are an entrepeneur. You can chnage the world!');
+            }
+        }
+    }
+} else if(gender == 'female') {
+    console.log('Welcome in the female zone!');
+    if(age > 20) {
+        console.log('Your age is enough  to jump the next plane!');
+        if(student == true) {
+            console.log('You are a student. It"s right time to start');
+            if(entrepeneur == true) {
+                console.log('You are an entrepeneur. You can chnage the world!');
+            }
+        }
+    }
+
+} else {
+    console.log('This project is only for male and female.');
+}
