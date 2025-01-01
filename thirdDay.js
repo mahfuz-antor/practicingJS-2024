ceeed9148e183ec86b9a2e5f19f972826093f813
mahfuz-antor <antorcnj2014@gmail.com>
2025-01-01 21:15:41 +0600

    multiple ternary example added

--- thirdDay.js
+++ thirdDay.js
@@ -9,3 +9,16 @@ const greetings = (person) =>  {
 }
 
 console.log(greetings({name: 'Mahfuz Antor'}));
+
+// multiple condition for ternary operator
+
+function getRealVoter(name) {
+    // console.log(name == number);
+    return name == 'Mahfuz' ? 'Your are the coder!' 
+    : name == 'Esrat'  ? 'You are the Genius housewife'
+    :name == 'Antor' ? 'The another name of Mahfuz'
+    : typeof name ==  'number' ? 'Please insert a text value'
+    : 'Please type the proper value'
+}
+
+console.log(getRealVoter(88));
